c2055dfd4a8934cdbcda9b2455dab744aa9198c5
Adrian Regenfuss <a.regenfuss@gmx.de>
2015-04-02 02:02:20 +0200

    Added better word checking.

--- gd.sh
+++ gd.sh
@@ -2,9 +2,12 @@
 curl $1 |
 tr -d '\n'|
 sed -e 's/<[^>]\+>/ /g'|
- tr '[\t ]' '\n'|
- strings|
- sed '/[^A-Za-z0-9]/d'|
- sort|
- uniq -c|
- sort -n > worte.txt
+sed -e 's/  */ /g'|
+sed -e 's/\t\t*/ /g'|
+tr '[\t ]' '\n'|
+sed -e '/ /d'|
+sed -e '/^$/d'|
+sed -e '/[^A-Za-z0-9]/d'|
+sort|
+uniq -c|
+sort -n
