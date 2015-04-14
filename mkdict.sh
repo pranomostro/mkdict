88e933f122ca4f9221c8fd1bf831448aedfc7aac
Adrian Regenfuss <a.regenfuss@gmx.de>
2015-04-14 23:23:12 +0200

    Improved performance (a lot)

--- mkdict.sh
+++ mkdict.sh
@@ -8,7 +8,8 @@ IFS='
 '
 for a in `seq 1 $2`; do
 	i=`sed -n "$a p" links`
-	./getl.sh $i $a $3 | ./merge.sh links
+	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then ./getl.sh $i $a $3 | ./merge.sh links
++       else curl $i > data/"$a"; fi
 done
 
 ./prwd.sh | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }' | sort -n >dict.txt
