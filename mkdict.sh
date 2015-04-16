d8827335517e2986460e7d7746a5e063390925b4
Adrian Regenfuss <a.regenfuss@gmx.de>
2015-04-16 19:01:25 +0200

    Placeholder commit

--- mkdict.sh
+++ mkdict.sh
@@ -9,7 +9,7 @@ IFS='
 for a in `seq 1 $2`; do
 	i=`sed -n "$a p" links`
 	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then ./getl.sh $i $a $3 | ./merge.sh links
-+       else curl $i > data/"$a"; fi
+       else curl $i > data/"$a"; fi
 done
 
 ./prwd.sh | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }' | sort -n >dict.txt
