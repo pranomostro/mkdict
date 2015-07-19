d6ad005be8e9b07bdf804204345f214315d5bd83
adrian <a.regenfuss@gmx.de>
2015-07-19 16:43:40 +0200

    Added a license, added a TODO-file.

--- mkdict.sh
+++ mkdict.sh
@@ -7,11 +7,11 @@ echo $1 >links
 IFS='
 '
 
-#for a in `seq 1 $2`; do
-#	i=`sed -n "$a p" links`
-#	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then ./getl.sh $i $a $3 | ./merge.sh links
-#	else curl $i > data/"$a"; fi
-#done
+for a in `seq 1 $2`; do
+	i=`sed -n "$a p" links`
+	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then ./getl.sh $i $a $3 | ./merge.sh links
+	else curl $i > data/"$a"; fi
+done
 
 ./prwd.sh | ./gd.sh | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }' | sort -n >dict.txt
 
