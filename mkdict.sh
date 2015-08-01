a6802d02bfe3bb42ad466b127add06ddd0b84dcc
scharlatan <a.regenfuss@gmx.de>
2015-08-01 21:55:11 +0200

    Refactored a bit.

--- mkdict.sh
+++ mkdict.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -ne 3 ]; then
+	echo "mkdict LINK COUNT" >/dev/stderr
+	exit 1
+fi
+
 touch links dict.txt
 
 echo $1 >links
@@ -16,7 +21,8 @@ for a in `seq 1 $2`; do
 	fi
 done
 
-prwd.sh | gd | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }' | sort -n >dict.txt
+prwd.sh | gd | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }'
+| sort -n >dict.txt
 
 n=`awk '{ a+=$1 } END { print a }' dict.txt `
 awk "{ print (\$1/$n)*1000000 \" \" \$2}" dict.txt > dict
@@ -28,7 +34,7 @@ rm links
 function getl()
 {
 	curl "$1" | tee data/"$2" | tr '\n' ' ' | grep -o '<a href="[^"]*"' |
-	sed 's/^<a href="//;s/"$//' | grep -E "$3" | sed 's/.*/https:\/\/de.wikipedia.org&/'
+	sed 's/^<a href="//;s/"$//'
 }
 
 function gd()
@@ -37,20 +43,7 @@ function gd()
 	tr -s '\t ' '\n'|
 	9 grep '^[a-zA-ZüöäÜÖÄßẞ][a-züöäß]+$'|
 	ag -v '([a-zA-ZüöäÜÖÄßẞ])\1{3}'|
-	grep -vE '^(break|case|catch|continue|debugger|
-	default|delete|do|else|false|finally|for|function|
-	if|in|instanceof|new|null|return|switch|this|
-	throw|true|try|typeof|var|void|while|with|abstract|
-	boolean|byte|char|class|const|double|enum|export|
-	extends|final|float|goto|implements|import|int|
-	interface|let|long|native|package|private|protected|
-	public|short|static|super|synchronized|throws|transient|
-	volatile|yield|alert|blur|closed|document|focus|
-	frames|history|innerHeight|innerWidth|length|location|
-	navigator|open|outerHeight|outerWidth|parent|screen|
-	screenX|screenY|statusbar|window|sync|cache|ibn|of|
-	at|en|fr|Fr|Sa|Mo|di|Mi|mi|Le|la|del|to|tsor|de|the|cache)$'
-#Javascript keywords which are clearly not german words
+	grep -fFv badwords.txt
 }
 
 function merge()
