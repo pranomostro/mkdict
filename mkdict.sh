42857b3b9ecb77017772a74f42ec80cc8fcdb498
scharlatan <a.regenfuss@gmx.de>
2015-08-01 21:19:52 +0200

    Made minor changes, nothing important.

--- mkdict.sh
+++ mkdict.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 touch links dict.txt
+
 echo $1 >links
+
 IFS='
 '
 
 for a in `seq 1 $2`; do
 	i=`sed -n "$a p" links`
-	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then getl $i $a $3 | merge links
-	else curl $i > data/"$a"; fi
+	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then
+		getl $i $a $3 | merge links
+	else
+		curl $i > data/"$a"
+	fi
 done
 
 prwd.sh | gd | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }' | sort -n >dict.txt
