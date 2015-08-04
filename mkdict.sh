91e9628a936f86914528da1a8c0bf36c98903d79
scharlatan <a.regenfuss@gmx.de>
2015-08-04 18:08:21 +0200

    Fixed mkdict.sh and configured it for Wikipedia.

--- mkdict.sh
+++ mkdict.sh
@@ -1,49 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-	echo "mkdict LINK COUNT" >/dev/stderr
-	exit 1
-fi
-
-touch links dict.txt
-
-echo $1 >links
-
-IFS='
-'
-
-for a in `seq 1 $2`; do
-	i=`sed -n "$a p" links`
-	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then
-		getl $i $a $3 | merge links
-	else
-		curl $i > data/"$a"
-	fi
-done
-
-prwd.sh | gd | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }'
-| sort -n >dict.txt
-
-n=`awk '{ a+=$1 } END { print a }' dict.txt `
-awk "{ print (\$1/$n)*1000000 \" \" \$2}" dict.txt > dict
-
-mv dict dict.txt
-
-rm links
-
 function getl()
 {
+#Replace the pattern in the last grep for configuration
 	curl "$1" | tee data/"$2" | tr '\n' ' ' | grep -o '<a href="[^"]*"' |
-	sed 's/^<a href="//;s/"$//'
+	sed 's/^<a href="//;s/"$//' | grep -E '^(\/)+wiki' |
+	grep -v ':' | sed 's/^/https:\/\/de.wikipedia.org/'
 }
 
 function gd()
 {
 	sed -ue 's/<[^>]\+>/ /g'|
 	tr -s '\t ' '\n'|
-	9 grep '^[a-zA-ZüöäÜÖÄßẞ][a-züöäß]+$'|
+	grep '^[a-zA-ZüöäÜÖÄßẞ][a-züöäß]\+$'|
 	ag -v '([a-zA-ZüöäÜÖÄßẞ])\1{3}'|
-	grep -fFv badwords.txt
+	grep -vFf badwords.txt
 }
 
 function merge()
@@ -56,9 +27,31 @@ function merge()
 	done
 }
 
-function prwd()
-{
-	for i in `tree -fi --norpeort data | grep -v '^data'`; do
-		cat $i
-	done
-}
+if [ $# -ne 2 ]; then
+	echo "mkdict LINK COUNT" >/dev/stderr
+	exit 1
+fi
+
+touch links dict.txt
+
+echo $1 >links
+
+IFS='
+'
+
+for a in `seq 1 "$2"`; do
+	i=`sed -n "$a p" links`
+	if [ `wc -l links | awk '{ print $1 }'` -le "$2" ]; then
+		getl "$i" "$a" | merge links
+	else
+		curl $i > data/"$a"
+	fi
+done
+
+cat data/* | gd | awk '{ n[$0]++ } END { for ( i in n ) { print n[i] " " i } }' | sort -n >dict.txt
+
+n=`wc -l dict.txt | awk '{ print $1 }'`
+awk "{ print (\$1/$n)*1000000 \" \" \$2}" dict.txt > dict
+
+mv dict dict.txt
+
