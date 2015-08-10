69d09f7d411158ba2b6063f8214d98259255f501
scharlatan <a.regenfuss@gmx.de>
2015-08-10 18:44:43 +0200

    Began to clean skip.c

--- skip.c
+++ skip.c
@@ -2,61 +2,64 @@
 #include <stdio.h>
 #include <string.h>
 
-/*TODO:
-	-when an EOF occurs anywhere, don't hang
-	 or print the EOF, just die.
-	-also, the code is ugly.
-*/
+void waitfor(char* end);
 
 int main(int argc, char** argv)
 {
 	char c;
-	unsigned int i=0, fin;
+	unsigned int i;
 
 	if(argc!=3)
 	{
-		fprintf(stderr, "error: skip [STR1] [STR2] expected, exiting.\n");
+		fprintf(stderr, "error: skip BEGIN END expected, exiting.\n");
 		exit(1);
 	}
 
 	char* begin=argv[1];
 	char* end=argv[2];
 
-	char* s=(char*) malloc(sizeof(char)*
+	char* buf=(char*) malloc(sizeof(char)*
 		(strlen(begin)>strlen(end)?strlen(begin):strlen(end))
 		);
 
 	do
 	{
-		start:
 		c=getchar();
 
 		if(c==*begin)
 		{
-			s[0]=c;
+			buf[0]=c;
 
 			for(i=1; begin[i]==(c=getchar()); i++)
-				s[i]=c;
-			s[i]='\0';
+				buf[i]=c;
+			buf[i]='\0';
+			ungetc(c, stdin);
 
 			if(i==strlen(begin))
-			{
-				ungetc(c, stdin);
-				fin=0;
-				while(!fin)
-				{
-					for(i=0; end[i]==(c=getchar()); i++);
-					if(i>=1)
-						ungetc(c, stdin);
-					if(i==strlen(end))
-						goto start;
-				}
-			}
+				waitfor(end);
 			else
-				printf(s);
+				printf(buf);
+			continue;
 		}
-		putchar(c);
+		if(c!=EOF)
+			putchar(c);
 	}while(c!=EOF);
 
+	free(buf);
+
 	return 0;
+}
+
+void waitfor(char* end)
+{
+	unsigned int i;
+	char c;
+	while(1)
+	{
+		for(i=0; end[i]==(c=getchar()); i++);
+		if(i>=1)
+			ungetc(c, stdin);
+		if(i==strlen(end))
+			return;
+	}
 }
