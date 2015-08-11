0536e0b8ad46691d3cd9c41f1ad62b63ec612531
scharlatan <a.regenfuss@gmx.de>
2015-08-11 16:10:43 +0200

    Removed the algorithm, going to write a new one.

--- skip.c
+++ skip.c
@@ -22,45 +22,9 @@ int main(int argc, char** argv)
 		(strlen(begin)>strlen(end)?strlen(begin):strlen(end))
 		);
 
-	do
-	{
-		c=getchar();
-
-		if(c==*begin)
-		{
-			buf[0]=c;
-
-			for(i=1; begin[i]==(c=getchar()); i++)
-				buf[i]=c;
-			buf[i]='\0';
-			ungetc(c, stdin);
-
-			if(i==strlen(begin))
-				waitfor(end);
-			else
-				printf(buf);
-			continue;
-		}
-		if(c!=EOF)
-			putchar(c);
-	}while(c!=EOF);
 
 	free(buf);
 
 	return 0;
 }
 
-int waitfor(char* end)
-{
-	unsigned int i;
-	char c;
-
-	while(1)
-	{
-		for(i=0; end[i]==(c=getchar()); i++);
-		if(i>=1)
-			ungetc(c, stdin);
-		if(i==strlen(end))
-			return 1;
-	}
-}
